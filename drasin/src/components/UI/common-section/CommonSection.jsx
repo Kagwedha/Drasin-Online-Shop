3dd9f87477efe2319544177f179dc9f46f10a37a
Kagwedha <kagwedhashikongo@gmal.com>
2022-07-05 19:12:28 +0200

    fifth commit

---
+++ drasin/src/components/UI/common-section/CommonSection.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container } from 'reactstrap';
+
+import "../../../styles/common-section.css";
+
+const CommonSection = (props) => {
+  return (
+    <section className='common_section'>
+        <Container>
+            <h2 className='text-white'>{props.title}</h2>
+        </Container>
+    </section>
+  )
+}
+
+export default CommonSection;
